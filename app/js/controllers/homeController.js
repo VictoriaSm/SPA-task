6eff693c736a9876ff4b5a45e685d8576af5fbc4
VictoriaSm <vikusha41j@gmail.com>
2017-10-11 19:21:10 +0300

    spa3

--- app/js/controllers/homeController.js
+++ app/js/controllers/homeController.js
@@ -1,34 +1,34 @@
-homeApp.controller('homeController', function ($scope, $http){
+homeApp.controller('homeController', ['$scope','$http',function (scope, $http){
 
         $http({method: 'GET', url: 'homes.json'}).
         then(function success(response) {
-            $scope.homes = response.data;
+            scope.homes = response.data;
         });
 
-        var inputAdd = angular.element( document.querySelector('#addName') );
-        var buttonAdd = angular.element( document.querySelector('.btn-add') );
-        $scope.addElem = function (elem) {
+
+        scope.addElem = function (elem) {
 
             if ( elem == undefined || elem == '' || elem.replace(/\s/g,'') == '' || elem.length < 3 ) {
-                inputAdd.addClass('error');
-                buttonAdd.addClass('error');
+                scope.error = true;
             }
             else {
-                $scope.homes.push({homeName:elem});
-                $scope.newName = '';
+                scope.homes.push({homeName:elem});
+                scope.newName = '';
+                scope.error = false;
             }
         };
 
-        $scope.removeClass = function () {
-            inputAdd.removeClass('error');
-            buttonAdd.removeClass('error');
-        };
+        scope.editElem = function (s) {
 
-        /*var inputEdit = angular.element( document.querySelector('#editName') );
-        var buttonEdit = angular.element( document.querySelector('.btn-edit') );*/
-        $scope.editElem = function (s) {
-            // $scope.editName = '+';
-            console.log($scope.editName);
+            scope.homes[0].homeName = 'test'
+            var a = s.homeName;
+            if ( a == undefined || a == '' || a.replace(/\s/g,'') == '' || a.length < 3 ) {
+                scope.errorClone = true;
+            }
+            else {
+                console.log(a);
+            }
+            console.log(s);
         };
-    }
+    }]
 );

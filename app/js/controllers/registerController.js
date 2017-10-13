e8e119ea181539206200691498917e699b4e9517
VictoriaSm <vikusha41j@gmail.com>
2017-10-13 11:01:06 +0300

    spaReg

--- app/js/controllers/registerController.js
+++ app/js/controllers/registerController.js
@@ -1,23 +1,22 @@
 homeApp.controller('registerController', registerController);
 
 registerController.$inject = ['$scope', '$location'];
-function registerController(scope, $location) {
-
+function registerController(scope, location) {
     scope.user = {};
 
-    scope.minlength = 8;
-
-    scope.register = function (userForm) {
-        if(userForm.$invalid){
-            return console.error('NOOOOOOO')
+    scope.register = function(userForm) {
+        if ( scope.user.login === undefined || scope.user.login === '' || scope.user.login.replace(/\s/g,'') === '' ) {
+            scope.loginError = true;
+        } else scope.loginError = false;
+        if ( userForm.$invalid ) {
+            return;
         }
-        if ( scope.user.password !== scope.user.confirmPassword ){
+        if ( scope.confirmPassword !== scope.user.password ) {
             scope.passwordError = true;
-        } else {
-            localStorage.setItem('user', JSON.stringify(scope.user));
-            $location.path('/');
+            return;
         }
 
-        console.log(scope.user.password.length);
-    };
+        localStorage.setItem('user', JSON.stringify(scope.user));
+        location.path('/');
+    }
 }
